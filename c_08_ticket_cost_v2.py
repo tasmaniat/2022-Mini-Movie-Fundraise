0c28412a5c8e4c75a7a379eb3806fb5d400e0f5f
tasmaniat <101750632+tasmaniat@users.noreply.github.com>
2024-02-19 14:52:00 +1300

    Add files via upload

--- c_08_ticket_cost_v2.py
+++ c_08_ticket_cost_v2.py
@@ -1,4 +1,21 @@
+# Next Steps
+# Add currency formatting & Headings
+# integrate this component with the base component
+# SHow students that they can use comments at the top
+# of a component at the # end of a period so that they
+# know what they got and what they need to do next
+# add this adea to the last discussion slide!
+
+# PS: n base component remember to calculate surcharge
+# once payment method has been chosen.
+
 import pandas
+
+
+# currency formatting function
+def currency(x):
+    return "${:.2f}".format(x)
+
 
 # dictionaries to hold ticket details
 all_names = ["a", "b", "c", "d", "e"]
@@ -12,6 +29,7 @@ mini_movie_dict = {
 }
 
 mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+mini_movie_frame = mini_movie_frame.set_index('Name')
 
 # Calculate the total ticket cost (ticket + surcharge)
 mini_movie_frame['Total'] = mini_movie_frame['Surcharge'] \
@@ -24,11 +42,20 @@ mini_movie_frame["Profit"] = mini_movie_frame['Ticket Price'] - 5
 total = mini_movie_frame['Total'].sum()
 profit = mini_movie_frame['Profit'].sum()
 
+# Currency Formatting (uses currency function)
+add_dollars = ['Ticket Price', 'Surcharge', 'Total', 'Profit']
+for var_item in add_dollars:
+    mini_movie_frame[var_item] = mini_movie_frame[var_item].apply(currency)
+
+print()
+print("----- Ticket Data -----")
+print()
 # output table with ticket data
 print(mini_movie_frame)
+
+print()
+print("----- Ticket Cost / Profit -----")
 
 # output total ticket sales and profit
 print("Total Tickets Sales: ${:.2f}".format(total))
 print("Total Profit: ${:.2f}".format(profit))
-
-
