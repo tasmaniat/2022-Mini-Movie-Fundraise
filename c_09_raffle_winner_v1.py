1464fd7b033374eb8f02c7e617795d8749b350a9
tasmaniat <101750632+tasmaniat@users.noreply.github.com>
2024-02-21 09:49:07 +1300

    raffle works as expected
    raffle chooses winner by randomly choosing a name of the names that bought a ticket

---
+++ c_09_raffle_winner_v1.py
@@ -0,0 +1,42 @@
+import pandas
+import random
+
+
+# dictionaries to hold ticket details
+all_names = ["a", "b", "c", "d", "e"]
+all_ticket_costs = [7.50, 7.50, 10.50, 10.50, 6.50]
+surcharge = [0, 0, 0.53, 0.53, 0]
+
+mini_movie_dict = {
+    "Name": all_names,
+    "Ticket Price": all_ticket_costs,
+    "Surcharge": surcharge
+}
+
+mini_movie_frame = pandas.DataFrame(mini_movie_dict)
+# mini_movie_frame = mini_movie_frame.set_index('Name')
+
+# Calculate the total ticket cost (ticket + surcharge)
+mini_movie_frame['Total'] = mini_movie_frame['Surcharge'] \
+                            + mini_movie_frame['Ticket Price']
+
+# calculate the profit for each ticket
+mini_movie_frame["Profit"] = mini_movie_frame['Ticket Price'] - 5
+
+# choose a winner form put names list
+winner_name = random.choice(all_names)
+
+# get position of winner name in list
+win_index = all_names.index(winner_name)
+
+# look up total amount won (ie: ticket price + surcharge)
+total_won = mini_movie_frame.at[win_index, 'Total']
+
+# set index at end (before printing)
+mini_movie_frame = mini_movie_frame.set_index('Name')
+print(mini_movie_frame)
+
+print()
+print("----- Raffle Winner -----")
+print("Congratulations {}. You have won ${} ie: your"
+      "ticket if free!".format(winner_name, total_won))
